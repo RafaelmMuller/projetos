9a767dfaa67ac26d4adda7975aec39c8b5f93e89
RafaelmMuller <rafaelmmuler@gmail.com>
2023-09-13 21:05:41 -0300

    add readme

--- .vscode/codigo.js
+++ .vscode/codigo.js
@@ -1,11 +1,12 @@
-function toggleMode(){
+function toggleMode() {
   const body = document.body
-  body.classList.toggle('light')
+  body.classList.toggle("light")
 
   const img = document.querySelector("#profile img")
 
-  if (body.classList.contains('light')){
-   img.setAtribute("src", "./assets/avatar-light.png")
-  } else {img.setAtribute("src","./assets/avatar.png")
-    }
+  if (body.classList.contains("light")) {
+    img.setAtribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAtribute("src", "./assets/avatar.png")
   }
+}
